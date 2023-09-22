69599fcd2a5127c9f54c3ad1a9e83f77d297eb98
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-09-22 12:51:08 +0530

    prisma + supabase added

---
+++ src/components/right-sheet.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button"
+import { AlignJustify, Link } from 'lucide-react'
+import { CategoriesType } from "@/types"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+import { Command, CommandItem, CommandList, CommandGroup } from "./ui/command"
+
+export function RightSheet({ categories }: { categories: CategoriesType[] }) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <button><AlignJustify /></button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Categories</SheetTitle>
+          <SheetDescription>
+            Click to explore.
+          </SheetDescription>
+        </SheetHeader>
+        <div>
+          <Command>
+            <CommandList>
+              <CommandGroup>
+                {categories.map(x => <CommandItem key={x.id}>{x.title}</CommandItem>)}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
