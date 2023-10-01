0db41430d2014bf789f41f390656e86f237c0c4c
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-10-01 20:53:04 +0530

    min-height added

--- src/app/(main)/cart/loading.tsx
+++ src/app/(main)/cart/loading.tsx
@@ -2,7 +2,7 @@ import CartSkeleton from "@/components/cart-skeleton"
 
 export default function Loading(){
     return (
-        <div className="p-8 md:p-16">
+        <div className="p-8 md:p-16 min-h-screen">
             <CartSkeleton/>
             <CartSkeleton/>
             <CartSkeleton/>
