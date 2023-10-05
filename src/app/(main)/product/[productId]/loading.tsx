1c11c3d2f19087b240e4d51bb82a5bb066306078
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-10-06 00:38:13 +0530

    padding added

--- src/app/(main)/product/[productId]/loading.tsx
+++ src/app/(main)/product/[productId]/loading.tsx
@@ -2,11 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading(){
     return (
-        <>
+        <div className="p-8">
         <Skeleton className="w-80 h-80"/>
         <Skeleton className="h-4 w-10"/>
         <Skeleton className="h-4 w-6"/>
 
-        </>
+        </div>
     )
 }
