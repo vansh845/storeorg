0e6b1f8cd89bad7c7f1dcd7d44e42607354ed7fa
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-10-14 19:35:21 +0530

    C:/Program Files/Git/products working

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -1,15 +1,19 @@
+import ProductCard from "@/components/product-card"
 import { prisma } from "../../../../prisma"
 
-export default async function Products(){
+export default async function Products() {
 
-    const data = await prisma.stores.findMany({
-        include:{
-            products:true
-        }
-    })
-    console.log(data)
+    const data = await prisma.products.findMany()
+    const products = data.map(product => <ProductCard product={product} />)
 
     return (
-        <p>hi</p>
+        <div className="min-h-screen p-5 space-y-5">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Products
+            </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {products}
+            </div>
+        </div>
     )
 }
